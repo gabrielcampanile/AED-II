bf6cc60f9615d1c64e17f15f27d474a371c954e8
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2024-04-10 15:24:05 -0300

    Ordenação em Tempo Linear

---
+++ 04.10 - Ordenação em Tempo  Linear/CountingSort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void countingSort(int *A, int *B, int k, int n) { //input: A, k(maior elemento de A) ; output: B
+    int i, j;
+
+    int *C = (int *)malloc((k + 1)*sizeof(int));
+
+    for(i=0; i<=k; i++) {
+        C[i] = 0;
+    }
+
+    for(j=1; j<=n; j++) {
+        C[A[j]] = C[A[j]] + 1; //núm.elem.de tamanho i
+    }
+
+    printf("C array: ");
+    for (int i = 0; i <= k; i++) {
+        printf("%i ", C[i]);
+    }
+    printf("\n");
+
+    for(int a=1; a<=k; a++) {
+        C[a] = C[a] + C[a-1]; // soma prefixa
+    }
+
+    printf("C array: ");
+    for (int i = 0; i <= k; i++) {
+        printf("%i ", C[i]);
+    }
+    printf("\n");
+
+    for(j=n; j>=1; j--) {
+        B[C[A[j]]] = A[j];
+        C[A[j]] = C[A[j]] - 1;
+    }
+}
+int main() {
+    int n, k;
+    printf("Size: ");
+    scanf("%d", &n);
+
+    int *A = (int *)malloc((n + 1) * sizeof(int));
+    int *B = (int *)malloc((n + 1) * sizeof(int));
+
+    printf("A: ");
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &A[i]);
+        if (i == 1 || A[i] > k) {
+            k = A[i];
+        }
+    }
+
+    printf("k = %i\n", k);
+
+    countingSort(A, B, k, n);
+
+    printf("B: ");
+    for (int i = 1; i <= n; i++) {
+        printf("%i ", B[i]);
+    }
+    printf("\n");
+
+    free(A);
+    free(B);
+
+    return 0;
+}
---
+++ 04.10 - Ordenação em Tempo  Linear/RadixSort.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void radixSort(int *A, int d) {
+    int i;
+
+    for(i=0; i<d; i++) {
+        
+    }
+}
